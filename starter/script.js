719a7e179e1248e84a2736601f597d2c44a3a354
Marcus Edralin <medralin36@yahoo.com>
2021-05-11 14:44:24 -0700

    first commit

---
+++ starter/script.js
@@ -0,0 +1,15 @@
+'use strict';
+// activates strict mode for all scripts
+// you can also activate strict mode for a specifc code or block
+// makes it easier to introduce bugs in our code
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true;
+if (hasDriversLicense) console.log('I can drive!');
+
+// Using strict mode it shows us why it is our output isn't occuring
+
+// const interface = 'Audio';
+// const private = 534;
+const if = 23;
